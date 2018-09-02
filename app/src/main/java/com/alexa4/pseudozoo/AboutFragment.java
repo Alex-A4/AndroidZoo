de07e362943109c32d50158406b592517078e1dc
Alex <alex.A4.25@yandex.ru>
2018-09-02 11:55:23 +0300

    Added networks buttons to visit zoo's networks

--- app/src/main/java/com/alexa4/pseudozoo/AboutFragment.java
+++ app/src/main/java/com/alexa4/pseudozoo/AboutFragment.java
@@ -1,5 +1,7 @@
 package com.alexa4.pseudozoo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -18,6 +21,50 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_about, container, false);
+
+        ImageView networks_vk = (ImageView) root.findViewById(R.id.networks_vk);
+        networks_vk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://vk.com/zooyar";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        ImageView networks_facebook = (ImageView) root.findViewById(R.id.networks_facebook);
+        networks_facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.facebook.com/yaroslavlzoo";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        ImageView networks_instagram = (ImageView) root.findViewById(R.id.networks_instagram);
+        networks_instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.instagram.com/yaroslavlzoo/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        ImageView networks_youtube = (ImageView) root.findViewById(R.id.networks_youtube);
+        networks_youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.youtube.com/channel/UC_2z1FBwooyqMcUTYrzjyiw/about";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
 
         return root;
     }
