9f14ec49adbe6a2365716846fadc5fa210be862f
Alex <alex.A4.25@yandex.ru>
2018-09-02 15:34:27 +0300

    Added internet permission

--- app/src/main/java/com/alexa4/pseudozoo/ZooMapFragment.java
+++ app/src/main/java/com/alexa4/pseudozoo/ZooMapFragment.java
@@ -3,23 +3,45 @@ package com.alexa4.pseudozoo;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 
 /**
  * MapFragment represent map where zoo is, which place visitor can visit
  */
-public class MapFragment extends Fragment {
+public class ZooMapFragment extends Fragment {
 
+    SupportMapFragment mMapFragment;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_map, container, false);
 
+        FragmentManager fm = getChildFragmentManager();
+
+        mMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+
+        if (mMapFragment == null){
+            mMapFragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, mMapFragment).commit();
+        }
+
+
+
         return root;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
     }
 }
