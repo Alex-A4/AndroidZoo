3decf31523d6ef6004821a11d0818bf328eea32f
Alex Adrianov <alex.a4.25@yandex.ru>
2018-09-12 22:08:13 +0300

    Changed structure. Was imageId, now imageUrl and Bitmap, added fullNewsLing which contains url on page with full news

--- app/src/main/java/com/alexa4/pseudozoo/user_data/News.java
+++ app/src/main/java/com/alexa4/pseudozoo/user_data/News.java
@@ -1,28 +1,34 @@
 package com.alexa4.pseudozoo.user_data;
 
 
+import android.graphics.Bitmap;
+
 /**
- * Custom class for using it in Adapter
+ * Custom class for using it list of news
  */
 public final class News{
     private String caption;
     private String time;
-    private int imageId;
+    private String imageUrl;
     private String description;
+    private String fullNewsLink;
+    private Bitmap bitmap;
 
-    public News(String caption, String time,int imageId, String description){
+    public News(String caption, String time, String image, String description, String fullNewsLink){
         this.caption = caption;
         this.time = time;
-        this.imageId = imageId;
+        this.imageUrl = image;
         this.description = description;
+        this.fullNewsLink = fullNewsLink;
+        this.bitmap = null;
     }
 
     public String getCaption() {
         return caption;
     }
 
-    public int getImage() {
-        return imageId;
+    public String getImageUrl() {
+        return imageUrl;
     }
 
     public String getDescription() {
@@ -33,9 +39,18 @@ public final class News{
         return time;
     }
 
+    public void setBitmap(Bitmap bitmap){
+        this.bitmap = bitmap;
+    }
+
+    public Bitmap getBitmap(){
+        return this.bitmap;
+    }
+
     @Override
     public String toString(){
-        return caption + " " + time;
+        return caption + " " + time + "\n" + description + "\n" +  imageUrl + "\n" +
+               fullNewsLink + "\n---------------------\n";
     }
 }
 
