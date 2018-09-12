a97c2764a97a84792d76743f798fb1d0dd878577
Alex Adrianov <alex.a4.25@yandex.ru>
2018-09-12 22:20:50 +0300

    Added methods to show and hide connecting textView, changed NewsAdapter initializing

--- app/src/main/java/com/alexa4/pseudozoo/activities_package/MainFragment.java
+++ app/src/main/java/com/alexa4/pseudozoo/activities_package/MainFragment.java
@@ -2,6 +2,7 @@ package com.alexa4.pseudozoo.activities_package;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -22,9 +23,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alexa4.pseudozoo.R;
+import com.alexa4.pseudozoo.adapters.BitmapAdapter;
+import com.alexa4.pseudozoo.models.ModelNews;
 import com.alexa4.pseudozoo.presenter.PresenterNews;
-import com.alexa4.pseudozoo.presenter.ViewInterface;
+import com.alexa4.pseudozoo.presenter.ViewInterfaceNews;
 import com.alexa4.pseudozoo.user_data.News;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -33,12 +38,12 @@ import java.util.ArrayList;
  * MainFragment contains list of news, toolbar with settings button
  * It's loading when the app starts
  */
-public class MainFragment extends Fragment implements ViewInterface {
+public class MainFragment extends Fragment implements ViewInterfaceNews {
     private ListView newsList;
 
     private ConstraintLayout toolbar;
     private ConstraintLayout fragmentMain;
-
+    private TextView connectingText;
     private ArrayList<News> newsArrayList;
 
     private PresenterNews presenterNews;
@@ -56,7 +61,7 @@ public class MainFragment extends Fragment implements ViewInterface {
         newsList = (ListView) root.findViewById(R.id.news_list);
         toolbar = (ConstraintLayout) root.findViewById(R.id.main_fragment_toolbar);
         fragmentMain = (ConstraintLayout) root.findViewById(R.id.fragment_main);
-
+        connectingText = (TextView) root.findViewById(R.id.main_fragment_text_connecting);
 
         //Initializing settings button from toolbar
         final ImageView settings = (ImageView) root.findViewById(R.id.main_fragment_toolbar_settings);
@@ -81,13 +86,44 @@ public class MainFragment extends Fragment implements ViewInterface {
 
 
     /**
+     * Show connecting text when news loading begin
+     */
+    public void showConnectingText(){
+        connectingText.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Hide connecting text when news loading finish
+     */
+    public void hideConnectingText(){
+        connectingText.setVisibility(View.INVISIBLE);
+    }
+
+
+    /**
+     * If error occur then show Toast with error
+     * @param progress code of error
+     */
+    public void showErrorConnection(int progress){
+        connectingText.setVisibility(View.INVISIBLE);
+        switch (progress) {
+            case ModelNews.Progress.CONNECTING_ERROR:   Toast.makeText(getContext(),
+                    R.string.check_internet, Toast.LENGTH_SHORT).show();
+            break;
+            case ModelNews.Progress.DOWNLOADING_ERROR: Toast.makeText(getContext(),
+                    R.string.downloading_error, Toast.LENGTH_SHORT).show();
+            break;
+        }
+    }
+
+    /**
      * Setting news list from Presenter
      * @param list of news
      */
     public void updateNewsList(ArrayList<News> list){
-        System.out.println("This is my list" +list);
-        this.newsArrayList.addAll(list);
-        System.out.println("List after process" + newsArrayList);
+        System.out.println("List had been updated");
+        this.newsArrayList = list;
+        newsList.setAdapter(new NewsAdapter(getContext(), R.layout.news_item, newsArrayList));
     }
 
     /**
@@ -95,8 +131,11 @@ public class MainFragment extends Fragment implements ViewInterface {
      */
     @Override
     public void createNewsList() {
-        presenterNews.getNewsList();
+        //Init list of news if it's not empty
+        this.newsArrayList = presenterNews.getNewsList();
+        System.out.println("List had been created");
 
+        System.out.println("Adapter running");
         newsList.setAdapter(new NewsAdapter(getContext(), R.layout.news_item, newsArrayList));
         newsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -128,6 +167,10 @@ public class MainFragment extends Fragment implements ViewInterface {
             super(context, textViewResourceId, objects);
         }
 
+
+        /**
+         * Setting the news_item by content of the news
+         */
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
@@ -136,10 +179,25 @@ public class MainFragment extends Fragment implements ViewInterface {
 
             TextView caption = (TextView) news.findViewById(R.id.news_item_caption);
             caption.setText(newsArrayList.get(position).getCaption());
+
             TextView time = (TextView) news.findViewById(R.id.news_item_time);
             time.setText(newsArrayList.get(position).getTime());
+
             ImageView image = (ImageView) news.findViewById(R.id.news_item_image);
-            image.setImageResource(newsArrayList.get(position).getImage());
+
+            //If image already downloaded then set it else download it by URL
+            if (newsArrayList.get(position).getBitmap() != null)
+                image.setImageBitmap(newsArrayList.get(position).getBitmap());
+            else BitmapAdapter.decodeBitmapFromUrl(
+                    newsArrayList.get(position).getImageUrl(), getResources(),
+                    new BitmapAdapter.DownloadImageCallback() {
+                        @Override
+                        public void onDownloadFinished(Bitmap bitmap) {
+                            image.setImageBitmap(bitmap);
+                            newsArrayList.get(position).setBitmap(bitmap);
+                        }
+                    });
+
             TextView description = (TextView) news.findViewById(R.id.news_item_description);
             description.setText(newsArrayList.get(position).getDescription());
 
@@ -172,6 +230,16 @@ public class MainFragment extends Fragment implements ViewInterface {
         createNewsList();
     }
 
+
+    /**
+     * Later will be added scroll updating, now update news if size is 0
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (presenterNews.getNewsList().size() == 0)
+            presenterNews.updateNewsList();
+    }
 
     /**
      * Setting colors which depends of nightMode variable
