d01389ab753182d384808553af9215a85c54021a
Alex Adrianov <alex.a4.25@yandex.ru>
2018-09-15 10:57:24 +0300

    Added presenter's part forFullNewsActivity

--- app/src/main/java/com/alexa4/pseudozoo/presenter/PresenterFullNews.java
+++ app/src/main/java/com/alexa4/pseudozoo/presenter/PresenterFullNews.java
@@ -5,25 +5,17 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 import com.alexa4.pseudozoo.activities_package.FullNewsActivity;
-import com.alexa4.pseudozoo.activities_package.NewsFragment;
 import com.alexa4.pseudozoo.models.ModelNews;
 import com.alexa4.pseudozoo.user_data.FullNews;
-import com.alexa4.pseudozoo.user_data.News;
 
-import java.util.ArrayList;
-
-
-/**
- * Presenter which implements interface to download news from the internet
- */
-public class PresenterNews extends PresenterParent{
+public class PresenterFullNews extends PresenterParent {
     private final ModelNews modelNews;
 
     /**
      * Setting model to presenter
      * @param model
      */
-    public PresenterNews(ModelNews model) {
+    public PresenterFullNews(ModelNews model) {
         this.modelNews = model;
     }
 
@@ -45,53 +37,46 @@ public class PresenterNews extends PresenterParent{
         super.detachView();
     }
 
+
     /**
-     * Downloading list of news from Zoo web link
+     * Downloading news content
+     * @param imageUrl
      */
-    public void updateNewsList(){
-        modelNews.loadNews(new ModelNews.LoadNewsCallback() {
+    public void updateFullNews(String imageUrl){
+        modelNews.loadFullNews(new ModelNews.LoadFullNewsCallback(){
+
             @Override
-            public void retrieveResult(ArrayList<News> list) {
+            public void retrieveResult(FullNews fullNews) {
                 if (getView() != null)
-                    ((NewsFragment) getView()).updateNewsList(list);
+                    ((FullNewsActivity) getView()).updateNews(fullNews);
             }
 
             @Override
             public NetworkInfo getInstanceNetworkInfo() {
-                ConnectivityManager cm = (ConnectivityManager) ((NewsFragment) getView())
-                        .getContext()
+                ConnectivityManager cm = (ConnectivityManager) ((FullNewsActivity) getView())
                         .getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo networkInfo = cm.getActiveNetworkInfo();
                 return networkInfo;
             }
 
-
             @Override
             public void startDownloading() {
                 if (getView() != null)
-                    ((NewsFragment) getView()).showConnectingText();
+                    ((FullNewsActivity) getView()).startDownloading();
             }
 
             @Override
             public void stopDownloading() {
                 if (getView() != null)
-                    ((NewsFragment) getView()).hideConnectingText();
+                    ((FullNewsActivity) getView()).stopDownloading();
             }
 
             @Override
             public void errorWhileDownloading(int progress) {
                 if (getView() != null)
-                    ((NewsFragment) getView()).showErrorConnection(progress);
+                    ((FullNewsActivity) getView()).errorWhileDownloading(progress);
             }
-        });
+        }, imageUrl);
     }
 
-
-    /**
-     * Getting basic list of news
-     * @return
-     */
-    public ArrayList<News> getNewsList(){
-        return modelNews.getNewsList();
-    }
 }
---
+++ app/src/main/java/com/alexa4/pseudozoo/presenter/ViewInterfaceFullNews.java
@@ -0,0 +1,6 @@
+package com.alexa4.pseudozoo.presenter;
+
+public interface ViewInterfaceFullNews extends ViewInterfaceParent {
+
+    void setPresenter(PresenterFullNews presenter);
+}
