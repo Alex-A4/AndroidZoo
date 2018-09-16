0dfd595c4dcb1bb63c6e520eb565db9d7c257fdc
Alex Adrianov <alex.a4.25@yandex.ru>
2018-09-16 18:08:08 +0300

    Changed logic, now it's contains Low and HightQuality links that support launch ImageViewer

--- app/src/main/java/com/alexa4/pseudozoo/user_data/FullNews.java
+++ app/src/main/java/com/alexa4/pseudozoo/user_data/FullNews.java
@@ -6,21 +6,30 @@ public class FullNews {
     private String title;
     private String fullText;
     private String imgUrl;
-    private ArrayList<String> listUrlOfImages;
+    private ArrayList<String> listUrlOfImagesHighQuality;
+    private ArrayList<String> listUrlOfImagesLowQuality;
+
+
+    public void setListUrlOfImagesHighQuality(ArrayList<String> listUrlOfImagesHighQuality) {
+
+        this.listUrlOfImagesHighQuality = listUrlOfImagesHighQuality;
+    }
+
+    public void setListUrlOfImagesLowQuality(ArrayList<String> listUrlOfImagesLowQuality) {
+        this.listUrlOfImagesLowQuality = listUrlOfImagesLowQuality;
+    }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setListUrlOfImages(ArrayList<String> listUrlOfImages) {
-        this.listUrlOfImages = listUrlOfImages;
-    }
 
     public FullNews(String title, String fullText, String imgUrl) {
         this.title = title;
         this.fullText = fullText;
         this.imgUrl = imgUrl;
-        this.listUrlOfImages = new ArrayList<>();
+        this.listUrlOfImagesLowQuality = new ArrayList<>();
+        this.listUrlOfImagesHighQuality = new ArrayList<>();
     }
 
     public String getTitle() {
@@ -36,7 +45,11 @@ public class FullNews {
         return imgUrl;
     }
 
-    public ArrayList<String> getListUrlOfImages() {
-        return listUrlOfImages;
+    public ArrayList<String> getListUrlOfImagesHighQuality() {
+        return listUrlOfImagesHighQuality;
+    }
+
+    public ArrayList<String> getListUrlOfImagesLowQuality() {
+        return listUrlOfImagesLowQuality;
     }
 }
