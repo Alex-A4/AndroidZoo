92a108ad5008df59a694ebe56061a55d8c8b514b
Alex Adrianov <alex.a4.25@yandex.ru>
2018-10-14 22:02:43 +0300

    Created ImagesStore class

---
+++ app/src/main/java/com/alexa4/pseudozoo/user_data/ImagesStore.java
@@ -0,0 +1,31 @@
+package com.alexa4.pseudozoo.user_data;
+
+import java.util.List;
+
+/**
+ * Singleton container which contains list of images url
+ * @author alexa4
+ */
+public class ImagesStore {
+    private static ImagesStore sStore;
+    private List<String> mUrls;
+
+    private ImagesStore(){
+    }
+
+
+    public void setUrls(List<String> urls) {
+        mUrls = urls;
+    }
+
+    public List<String> getUrls() {
+        return mUrls;
+    }
+
+    public static ImagesStore getStore() {
+        if (sStore == null)
+            sStore = new ImagesStore();
+
+        return sStore;
+    }
+}
