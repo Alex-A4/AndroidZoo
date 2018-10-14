b15d717a6ad09afffd7026ae807296d5ad4335a9
Alex Adrianov <alex.a4.25@yandex.ru>
2018-10-14 17:17:48 +0300

    Deleted night mode

--- app/src/main/java/com/alexa4/pseudozoo/presenter/ViewInterfaceParent.java
+++ app/src/main/java/com/alexa4/pseudozoo/presenter/ViewInterfaceParent.java
@@ -1,12 +1,4 @@
 package com.alexa4.pseudozoo.presenter;
 
-import com.alexa4.pseudozoo.user_data.NightMode;
-
-
-/**
- * Top hierarchy interface which contains nightMode
- * Later nightMode must be moved to model (when mode could be use to all fragments)
- */
 public interface ViewInterfaceParent {
-    NightMode nightMode = new NightMode();
 }
