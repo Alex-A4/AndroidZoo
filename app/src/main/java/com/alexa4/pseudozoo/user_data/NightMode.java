183e1c0c1522bdfd5c25ea71c7f183e5dbe5f823
Alex Adrianov <alex.a4.25@yandex.ru>
2018-10-14 17:24:55 +0300

    NightMode made singleton

--- app/src/main/java/com/alexa4/pseudozoo/user_data/NightMode.java
+++ app/src/main/java/com/alexa4/pseudozoo/user_data/NightMode.java
@@ -5,9 +5,10 @@ package com.alexa4.pseudozoo.user_data;
  * Data class which contains information does night mode activate or not
  */
 public class NightMode {
+    private static NightMode sMode;
     private boolean mode;
 
-    public NightMode(){
+    private NightMode(){
         this.mode = false;
     }
 
@@ -17,5 +18,12 @@ public class NightMode {
 
     public boolean getMode(){
         return this.mode;
+    }
+
+    public static NightMode getNightMode() {
+        if (sMode == null)
+            sMode = new NightMode();
+
+        return sMode;
     }
 }
