867308ab9098279a5214670b29f145d61524fa64
Alex Adrianov <alex.a4.25@yandex.ru>
2018-10-25 22:41:09 +0300

    Added header image url to listUrlOfImages of fullNews

--- app/src/main/java/com/alexa4/pseudozoo/activities_package/FullNewsActivity.java
+++ app/src/main/java/com/alexa4/pseudozoo/activities_package/FullNewsActivity.java
@@ -124,7 +124,6 @@ public class FullNewsActivity extends FragmentActivity implements ViewInterfaceF
      */
     public void updateNews(FullNews fullNews){
         this.fullNews = fullNews;
-        fullNews.getListUrlOfImages().add(0, fullNews.getImgUrl());
 
         toolbarTitle.setText(fullNews.getTitle());
         imageOfNews.downloadCompressedImageByUrl(fullNews.getImgUrl());
@@ -182,6 +181,7 @@ public class FullNewsActivity extends FragmentActivity implements ViewInterfaceF
             this.urlsList = urls;
             this.recyclerWidth = width;
             ImagesStore.getStore().setUrls(urls);
+            ImagesStore.getStore().getUrls().add(0, fullNews.getImgUrl());
         }
 
 
