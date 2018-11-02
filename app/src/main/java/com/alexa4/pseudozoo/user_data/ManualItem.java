eaea2b92244ca878625a2e14281472340cd8187f
Alex Adrianov <alex.a4.25@yandex.ru>
2018-11-02 20:16:03 +0300

    Added field which contains image

--- app/src/main/java/com/alexa4/pseudozoo/user_data/ManualItem.java
+++ app/src/main/java/com/alexa4/pseudozoo/user_data/ManualItem.java
@@ -1,14 +1,29 @@
 package com.alexa4.pseudozoo.user_data;
 
+import android.graphics.Bitmap;
+
 public class ManualItem {
+    //The title of manual item
     private final String mTitle;
+    //The image source of manual item
     private final String mImageSrc;
+    //The url of manual item
     private final String mUrl;
+    //The image of manual item
+    private Bitmap mImage;
 
     public ManualItem(String title, String imageSrc, String url) {
         mTitle = title;
         mImageSrc = imageSrc;
         mUrl = url;
+    }
+
+    public Bitmap getImage() {
+        return mImage;
+    }
+
+    public void setImage(Bitmap image) {
+        mImage = image;
     }
 
     public String getUrl() {
