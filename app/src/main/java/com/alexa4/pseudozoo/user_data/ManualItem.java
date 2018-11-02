87c139ee544418a7868f0f59fc308b32eb65df37
Alex Adrianov <alex.a4.25@yandex.ru>
2018-11-02 18:42:06 +0300

    Added field to store url of manual item

--- app/src/main/java/com/alexa4/pseudozoo/user_data/ManualItem.java
+++ app/src/main/java/com/alexa4/pseudozoo/user_data/ManualItem.java
@@ -3,10 +3,16 @@ package com.alexa4.pseudozoo.user_data;
 public class ManualItem {
     private final String mText;
     private final String mImageSrc;
+    private final String mUrl;
 
-    public ManualItem(String text, String imageSrc) {
+    public ManualItem(String text, String imageSrc, String url) {
         mText = text;
         mImageSrc = imageSrc;
+        mUrl = url;
+    }
+
+    public String getUrl() {
+        return mUrl;
     }
 
     public String getText() {
