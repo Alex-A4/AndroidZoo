270833a0aed9907896eba449283075bf4ef8d4dc
Alex Adrianov <alex.a4.25@yandex.ru>
2018-11-02 15:59:31 +0300

    Added template of downloading

--- app/src/main/java/com/alexa4/pseudozoo/models/ModelManual.java
+++ app/src/main/java/com/alexa4/pseudozoo/models/ModelManual.java
@@ -1,6 +1,7 @@
 package com.alexa4.pseudozoo.models;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 
 /**
@@ -20,5 +21,55 @@ public class ModelManual {
             sManual = new ModelManual(context);
 
         return sManual;
+    }
+
+
+    /**
+     * Callback which notify about result of downloading
+     */
+    public interface DownloadManualCallback {
+        void sendResult(boolean result);
+    }
+
+    /**
+     * Method which call async downloading of manual from Zoo-web
+     * @param callback the callback
+     */
+    public void downloadManual(DownloadManualCallback callback) {
+        AsyncManualDownloading async = new AsyncManualDownloading(callback);
+        async.execute();
+    }
+
+
+    /**
+     * Class which implements async downloading of manuals
+     */
+    private static class AsyncManualDownloading extends AsyncTask<Void, Void, Boolean> {
+
+        private DownloadManualCallback mCallback;
+
+        public AsyncManualDownloading(DownloadManualCallback callback) {
+            mCallback = callback;
+        }
+
+        /**
+         * Download information about manual from http://yar-zoo.ru/animals.html
+         * @return the result of downloading
+         */
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+
+
+            return true;
+        }
+
+        /**
+         * Notify callback about downloading finish
+         * @param aBoolean the result of downloading
+         */
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            mCallback.sendResult(aBoolean);
+        }
     }
 }
