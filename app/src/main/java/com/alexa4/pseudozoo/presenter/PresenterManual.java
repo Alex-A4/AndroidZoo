2d8fbd66b22f20d50fb8a76b52e504c783da01bb
Alex Adrianov <alex.a4.25@yandex.ru>
2018-11-02 15:22:22 +0300

    Added constructor with initializing of model and view

--- app/src/main/java/com/alexa4/pseudozoo/presenter/PresenterManual.java
+++ app/src/main/java/com/alexa4/pseudozoo/presenter/PresenterManual.java
@@ -1,5 +1,15 @@
 package com.alexa4.pseudozoo.presenter;
 
+import com.alexa4.pseudozoo.activities_package.ManualFragment;
+import com.alexa4.pseudozoo.models.ModelManual;
+
 public class PresenterManual {
-    
+    private ManualFragment mView;
+    private ModelManual mModel;
+
+
+    public PresenterManual(ManualFragment fragment) {
+        mView = fragment;
+        mModel = ModelManual.getManual(mView.getContext());
+    }
 }
