0588f049315dc15c7d065d0b921a11d552dec4e0
Alex <alex.A4.25@yandex.ru>
2018-11-03 21:56:24 +0300

    Added adapter for animals recyclerview

--- app/src/main/java/com/alexa4/pseudozoo/activities_package/manual_views/ManualAnimalsFragment.java
+++ app/src/main/java/com/alexa4/pseudozoo/activities_package/manual_views/ManualAnimalsFragment.java
@@ -1,5 +1,7 @@
 package com.alexa4.pseudozoo.activities_package.manual_views;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,8 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alexa4.pseudozoo.R;
+import com.alexa4.pseudozoo.adapters.ImageCompressor;
 import com.alexa4.pseudozoo.models.ModelManual;
 import com.alexa4.pseudozoo.user_data.manual_data.ManualAnimalItem;
 import com.alexa4.pseudozoo.user_data.manual_data.ManualAnimalsStore;
@@ -62,15 +66,73 @@ public class ManualAnimalsFragment extends Fragment {
 
         //Downloading data of page
         ModelManual.downloadAnimalsPage(mAnimalsTypeItem.getUrl(), new ModelManual.DownloadAnimalsCallback() {
-            //TODO: add setting adapter
             @Override
             public void sendResult(boolean response) {
                 mAnimalsList = ManualAnimalsStore.getStore().getItems();
-                mAnimalsRV.setAdapter(null);
+                mAnimalsRV.setAdapter(new AnimalsAdapter(mAnimalsList));
             }
         });
 
         return root;
+    }
+
+    private class AnimalsAdapter extends RecyclerView.Adapter<AnimalsAdapter.AnimalsViewHolder> {
+        private ArrayList<ManualAnimalItem> mItems;
+
+        public AnimalsAdapter(ArrayList<ManualAnimalItem> items) {
+            mItems = items;
+        }
+
+        @NonNull
+        @Override
+        public AnimalsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View root = LayoutInflater.from(getContext()).inflate(R.layout.manual_list_item,
+                    viewGroup, false);
+
+            return new AnimalsViewHolder(root);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AnimalsViewHolder holder, @SuppressLint("RecyclerView") int position) {
+            holder.mTextView.setText(mItems.get(position).getTitle());
+
+            if (mItems.get(position).getImage() != null)
+                holder.mImageView.setImageBitmap(mItems.get(position).getImage());
+            else {
+                ImageCompressor.getCompressedImage(getContext(),
+                        mItems.get(position).getImageSrc(),
+                        new ImageCompressor.BitmapCompressorCallback() {
+                            @Override
+                            public void sendCompressedBmp(Bitmap bmp) {
+                                mItems.get(holder.getAdapterPosition()).setImage(bmp);
+                                holder.mImageView.setImageBitmap(bmp);
+                            }
+                        });
+            }
+
+            //TODO: change logic onClickListener
+            holder.mImageView.getRootView().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), mItems.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+
+        public class AnimalsViewHolder extends RecyclerView.ViewHolder {
+            private ImageView mImageView;
+            private TextView mTextView;
+            public AnimalsViewHolder(@NonNull View itemView) {
+                super(itemView);
+                mImageView = (ImageView) itemView.findViewById(R.id.manual_list_image);
+                mTextView = (TextView) itemView.findViewById(R.id.manual_list_text);
+            }
+        }
     }
 
     /**
