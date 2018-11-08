4818d8c1744b94277ba560d5148b95cdd2f73c9c
Alex <alex.A4.25@yandex.ru>
2018-11-08 13:29:23 +0300

    Added implementation of download callback

--- app/src/main/java/com/alexa4/pseudozoo/activities_package/manual_views/AnimalsFragment.java
+++ app/src/main/java/com/alexa4/pseudozoo/activities_package/manual_views/AnimalsFragment.java
@@ -1,12 +1,12 @@
 package com.alexa4.pseudozoo.activities_package.manual_views;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +15,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alexa4.pseudozoo.R;
+import com.alexa4.pseudozoo.adapters.ImageCompressor;
+import com.alexa4.pseudozoo.models.ModelManual;
+import com.alexa4.pseudozoo.user_data.manual_data.Animal;
 
 public class AnimalsFragment extends Fragment {
     private static final String ANIMAL_URL = "ANIMAL_URL";
@@ -22,7 +25,7 @@ public class AnimalsFragment extends Fragment {
     private TextView mPageText;
     private ImageView mPageImage;
     private ViewPager mPager;
-    private FragmentPagerAdapter mAdapter;
+    private Animal mAnimal;
 
     /**
      * Getting web url from args
@@ -54,13 +57,29 @@ public class AnimalsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_animals, container, false);
 
         mPageImage = (ImageView) root.findViewById(R.id.fragment_animals_image);
-        mPageImage.setImageDrawable(getResources().getDrawable(R.drawable.instagram));
 
         mPageText = (TextView) root.findViewById(R.id.fragment_animals_text);
-        mPageText.setText("There are text");
 
         mPager = (ViewPager) root.findViewById(R.id.fragment_animals_view_pager);
-        mPager.setAdapter(new AnimalsPagerAdapter(getFragmentManager()));
+
+        //Downloading info
+        ModelManual.downloadAnimalInfo(mAnimalUrl, new ModelManual.DownloadAnimalInfoCallback() {
+            @Override
+            public void sendResult(Animal animal) {
+                mAnimal = animal;
+
+                mPager.setAdapter(new AnimalsPagerAdapter(getFragmentManager()));
+                mPageText.setText(mAnimal.getDescription());
+
+                ImageCompressor.getCompressedImage(getContext(), mAnimal.getImageUrl(),
+                        new ImageCompressor.BitmapCompressorCallback() {
+                            @Override
+                            public void sendCompressedBmp(Bitmap bmp) {
+                                mPageImage.setImageBitmap(bmp);
+                            }
+                        });
+            }
+        });
 
         return root;
     }
@@ -77,18 +96,18 @@ public class AnimalsFragment extends Fragment {
         //TODO: add insert parameter
         @Override
         public Fragment getItem(int position) {
-            return TextAnimalsFragment.newInstance("Hi " + position);
+            return TextAnimalsFragment.newInstance(mAnimal.getParamText().get(position));
         }
 
         @Override
         public int getCount() {
-            return 5;
+            return mAnimal.getParamNames().size();
         }
 
         @Nullable
         @Override
         public CharSequence getPageTitle(int position) {
-            return "Page " + position;
+            return mAnimal.getParamNames().get(position);
         }
     }
 }
