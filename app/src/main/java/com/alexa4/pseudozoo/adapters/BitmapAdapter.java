c69697a045bdd896f261f4fadb8cba50b67be904
Alex <alex.A4.25@yandex.ru>
2018-11-15 19:42:55 +0300

    Added method to make image with rounded corners

--- app/src/main/java/com/alexa4/pseudozoo/adapters/BitmapAdapter.java
+++ app/src/main/java/com/alexa4/pseudozoo/adapters/BitmapAdapter.java
@@ -3,6 +3,12 @@ package com.alexa4.pseudozoo.adapters;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.media.Image;
 import android.os.AsyncTask;
 import android.widget.ImageView;
@@ -25,8 +31,9 @@ public class BitmapAdapter extends BitmapFactory{
      * @param res link to project Resources
      * @param callback callback interface which notify that download finished
      */
-    public static void decodeBitmapFromUrl(String url, Resources res, DownloadImageCallback callback) {
-        AsyncDownloadBitmap downloadBitmap = new AsyncDownloadBitmap(url, res, callback);
+    public static void decodeBitmapFromUrl(String url, Resources res, boolean isRoundCorners,
+                                           DownloadImageCallback callback) {
+        AsyncDownloadBitmap downloadBitmap = new AsyncDownloadBitmap(url, res, isRoundCorners, callback);
         downloadBitmap.execute();
     }
 
@@ -44,14 +51,17 @@ public class BitmapAdapter extends BitmapFactory{
      */
     static class AsyncDownloadBitmap extends AsyncTask<Void, Void, Bitmap> {
 
-        String url;
-        Resources res;
-        DownloadImageCallback callback;
+        private String url;
+        private Resources res;
+        private DownloadImageCallback callback;
+        private boolean mIsRoundCorners;
 
-        AsyncDownloadBitmap(String url, Resources res, DownloadImageCallback callback){
+        AsyncDownloadBitmap(String url, Resources res, boolean isRoundCorners,
+                            DownloadImageCallback callback){
             this.url = url;
             this.res = res;
             this.callback = callback;
+            mIsRoundCorners = isRoundCorners;
         }
 
         @Override
@@ -66,7 +76,38 @@ public class BitmapAdapter extends BitmapFactory{
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
+            if (mIsRoundCorners)
+                bitmap = getRoundedCornerBitmap(bitmap);
+
             callback.onDownloadFinished(bitmap);
         }
+    }
+
+
+    /**
+     * Method to get image with round corners
+     * @param bitmap the image which need to round
+     * @return the round bitmap
+     */
+    private static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 25;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
     }
 }
--- app/src/main/java/com/alexa4/pseudozoo/adapters/ImageCompressor.java
+++ app/src/main/java/com/alexa4/pseudozoo/adapters/ImageCompressor.java
@@ -3,6 +3,12 @@ package com.alexa4.pseudozoo.adapters;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.AsyncTask;
 
 import com.alexa4.pseudozoo.R;
@@ -18,13 +24,21 @@ public class ImageCompressor {
         void sendCompressedBmp(Bitmap bmp);
     }
 
-    public static void getCompressedImage(Context context, String url,
+    /**
+     * Getting compressed image by the url
+     * @param context the context of app
+     * @param url the url of image
+     * @param isRoundCorners the value which define rounded corners
+     * @param callback the callback to return image
+     */
+    public static void getCompressedImage(Context context, String url, boolean isRoundCorners,
                                           BitmapCompressorCallback callback) {
-        BitmapAdapter.decodeBitmapFromUrl(url, context.getResources(),
+        BitmapAdapter.decodeBitmapFromUrl(url, context.getResources(), false,
                 new BitmapAdapter.DownloadImageCallback() {
             @Override
             public void onDownloadFinished(Bitmap bitmap) {
-                AsyncImageCompressor compressor = new AsyncImageCompressor(context, bitmap, callback);
+                AsyncImageCompressor compressor = new AsyncImageCompressor(context, bitmap,
+                        isRoundCorners, callback);
                 compressor.execute();
             }
         });
@@ -35,11 +49,14 @@ public class ImageCompressor {
         private BitmapCompressorCallback callback;
         private Context context;
         private Bitmap bmp;
+        private boolean mIsRoundedCorners;
 
-        public AsyncImageCompressor(Context context, Bitmap bmp, BitmapCompressorCallback callback) {
+        public AsyncImageCompressor(Context context, Bitmap bmp, boolean isRoundedCorners,
+                                    BitmapCompressorCallback callback) {
             this.callback = callback;
             this.context = context;
             this.bmp = bmp;
+            mIsRoundedCorners = isRoundedCorners;
         }
 
         @Override
@@ -66,6 +83,10 @@ public class ImageCompressor {
                 Bitmap bitmap = new Compressor(context)
                         .setQuality(10)
                         .compressToBitmap(file);
+
+                if (mIsRoundedCorners)
+                    return getRoundedCornerBitmap(bitmap);
+
                 return bitmap;
             } catch (Exception e) {
                 //If error appeared
@@ -78,5 +99,33 @@ public class ImageCompressor {
         protected void onPostExecute(Bitmap bitmap) {
             callback.sendCompressedBmp(bitmap);
         }
+    }
+
+
+    /**
+     * Method to get image with round corners
+     * @param bitmap the image which need to round
+     * @return the round bitmap
+     */
+    private static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 25;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
     }
 }
